2457be2571aae45a33631cbee14c3da726cd07b1
Tim Jensen <Tim.Jensen@vector.com>
2024-12-03 09:03:10 +0100

    init: day-04

---
+++ day-04/src/bin/part1.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let input = include_str!("../input.txt").trim();
+    println!("Part 1: {}", process_input(input));
+}
+
+fn process_input(input: &str) -> usize {
+    43
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_input() {
+        let input = include_str!("../test_input.txt").trim();
+        assert_eq!(process_input(input), 42);
+    }
+}
