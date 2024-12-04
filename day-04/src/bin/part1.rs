6acb00b9518f16d08fa52997c09514c1c9a8d2a3
Tim Jensen <Tim.Jensen@vector.com>
2024-12-04 08:57:13 +0100

    feat: finish day-04

--- day-04/src/bin/part1.rs
+++ day-04/src/bin/part1.rs
@@ -1,10 +1,106 @@
+use grid::Grid;
+
+const PATTERN: &str = "XMAS";
+
 fn main() {
     let input = include_str!("../input.txt").trim();
     println!("Part 1: {}", process_input(input));
 }
 
 fn process_input(input: &str) -> usize {
-    43
+    let mut grid = Grid::new(0, 0);
+    input
+        .lines()
+        .for_each(|line| grid.push_row(line.chars().collect()));
+
+    let mut sum = 0;
+
+    // Rows
+    sum += grid
+        .iter_rows()
+        .map(|c| find_xmas_in_line(&c.collect::<String>()))
+        .sum::<usize>();
+    sum += grid
+        .iter_rows()
+        .map(|c| find_xmas_in_line(&c.rev().collect::<String>()))
+        .sum::<usize>();
+
+    // Columns
+    sum += grid
+        .iter_cols()
+        .map(|c| find_xmas_in_line(&c.collect::<String>()))
+        .sum::<usize>();
+    sum += grid
+        .iter_cols()
+        .map(|c| find_xmas_in_line(&c.rev().collect::<String>()))
+        .sum::<usize>();
+
+    // Diagonals
+    let diagonals = get_diagonals(&grid);
+    sum += diagonals
+        .iter()
+        .map(|d| find_xmas_in_line(d))
+        .sum::<usize>();
+    sum += diagonals
+        .iter()
+        .map(|d| find_xmas_in_line(&d.chars().rev().collect::<String>()))
+        .sum::<usize>();
+
+    sum
+}
+
+fn find_xmas_in_line(line: &str) -> usize {
+    let mut sum = 0;
+
+    for i in 0..=line.len() - PATTERN.len() {
+        if &line[i..i + PATTERN.len()] == PATTERN {
+            sum += 1;
+        }
+    }
+
+    sum
+}
+
+fn get_diagonals(grid: &Grid<char>) -> Vec<String> {
+    let mut diagonals: Vec<String> = vec![];
+
+    // Top-left to bottom-right diagonals
+    for c in 0..grid.cols() {
+        let mut diagonal = String::new();
+        for (i, j) in (0..grid.rows()).zip(c..grid.cols()) {
+            diagonal.push(grid[(i, j)]);
+        }
+        diagonals.push(diagonal);
+    }
+
+    for c in 1..grid.rows() {
+        let mut diagonal = String::new();
+        for (i, j) in (c..grid.rows()).zip(0..grid.cols()) {
+            diagonal.push(grid[(i, j)]);
+        }
+        diagonals.push(diagonal);
+    }
+
+    // Top-right to bottom-left diagonals
+    for c in 0..grid.cols() {
+        let mut diagonal = String::new();
+        for (i, j) in (0..grid.rows()).zip((0..=c).rev()) {
+            diagonal.push(grid[(i, j)]);
+        }
+        diagonals.push(diagonal);
+    }
+
+    for c in 1..grid.rows() {
+        let mut diagonal = String::new();
+        for (i, j) in (c..grid.rows()).zip((0..grid.cols()).rev()) {
+            diagonal.push(grid[(i, j)]);
+        }
+        diagonals.push(diagonal);
+    }
+
+    diagonals.retain(|d| d.len() >= PATTERN.len());
+
+    diagonals
 }
 
 #[cfg(test)]
@@ -14,6 +110,6 @@ mod test {
     #[test]
     fn test_input() {
         let input = include_str!("../test_input.txt").trim();
-        assert_eq!(process_input(input), 42);
+        assert_eq!(process_input(input), 18);
     }
 }
--- day-04/src/bin/part2.rs
+++ day-04/src/bin/part2.rs
@@ -1,10 +1,34 @@
+use grid::Grid;
+
 fn main() {
     let input = include_str!("../input.txt").trim();
     println!("Part 2: {}", process_input(input));
 }
 
 fn process_input(input: &str) -> usize {
-    43
+    let mut grid = Grid::new(0, 0);
+    input
+        .lines()
+        .for_each(|line| grid.push_row(line.chars().collect()));
+
+    grid.insert_row(0, vec!['0'; grid.cols()]);
+    grid.insert_col(0, vec!['0'; grid.rows()]);
+    grid.push_row(vec!['0'; grid.cols()]);
+    grid.push_col(vec!['0'; grid.rows()]);
+
+    let mut sum = 0;
+    for ((row, col), c) in grid.indexed_iter() {
+        if *c == 'A'
+            && ((grid[(row - 1, col - 1)] == 'M' && grid[(row + 1, col + 1)] == 'S')
+                || (grid[(row - 1, col - 1)] == 'S' && grid[(row + 1, col + 1)] == 'M'))
+            && ((grid[(row + 1, col - 1)] == 'M' && grid[(row - 1, col + 1)] == 'S')
+                || (grid[(row + 1, col - 1)] == 'S' && grid[(row - 1, col + 1)] == 'M'))
+        {
+            sum += 1;
+        }
+    }
+
+    sum
 }
 
 #[cfg(test)]
@@ -14,6 +38,6 @@ mod test {
     #[test]
     fn test_input() {
         let input = include_str!("../test_input.txt").trim();
-        assert_eq!(process_input(input), 42);
+        assert_eq!(process_input(input), 9);
     }
 }
