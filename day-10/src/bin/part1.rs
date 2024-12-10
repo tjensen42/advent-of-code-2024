4e54550b96afcb60e69939f38421cb93751e274b
Tim Jensen <Tim.Jensen@vector.com>
2024-12-10 08:49:04 +0100

    feat: finish day-10

---
+++ day-10/src/bin/part1.rs
@@ -0,0 +1,65 @@
+use std::collections::HashSet;
+
+use grid::Grid;
+
+fn main() {
+    let input = include_str!("../input.txt").trim();
+    println!("Part 1: {}", process_input(input));
+}
+
+fn process_input(input: &str) -> usize {
+    let mut map: Grid<u32> = Grid::new(input.lines().count(), 0);
+
+    for line in input.lines() {
+        map.push_row(line.chars().map(|c| c.to_digit(10).unwrap()).collect());
+    }
+
+    map.insert_row(0, vec![u32::MAX; map.cols()]);
+    map.insert_col(0, vec![u32::MAX; map.rows()]);
+    map.push_row(vec![u32::MAX; map.cols()]);
+    map.push_col(vec![u32::MAX; map.rows()]);
+
+    let trailheads: Vec<_> = map
+        .indexed_iter()
+        .filter_map(|(pos, &c)| if c == 0 { Some(pos) } else { None })
+        .collect();
+
+    trailheads
+        .iter()
+        .map(|&pos| {
+            let mut reached_tails = HashSet::new();
+            find_valid_tails(&map, pos, &mut reached_tails);
+            reached_tails.len()
+        })
+        .sum()
+}
+
+fn find_valid_tails(map: &Grid<u32>, pos: (usize, usize), tails: &mut HashSet<(usize, usize)>) {
+    let new_poses = &[
+        (pos.0 + 1, pos.1),
+        (pos.0 - 1, pos.1),
+        (pos.0, pos.1 + 1),
+        (pos.0, pos.1 - 1),
+    ];
+
+    for new_pos in new_poses {
+        if map[*new_pos] == map[pos] + 1 {
+            if map[*new_pos] == 9 {
+                tails.insert(*new_pos);
+            } else {
+                find_valid_tails(map, *new_pos, tails);
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_input() {
+        let input = include_str!("../test_input.txt").trim();
+        assert_eq!(process_input(input), 36);
+    }
+}
---
+++ day-10/src/bin/part2.rs
@@ -0,0 +1,62 @@
+use grid::Grid;
+
+fn main() {
+    let input = include_str!("../input.txt").trim();
+    println!("Part 2: {}", process_input(input));
+}
+
+fn process_input(input: &str) -> usize {
+    let mut map: Grid<u32> = Grid::new(input.lines().count(), 0);
+
+    for line in input.lines() {
+        map.push_row(line.chars().map(|c| c.to_digit(10).unwrap()).collect());
+    }
+
+    map.insert_row(0, vec![u32::MAX; map.cols()]);
+    map.insert_col(0, vec![u32::MAX; map.rows()]);
+    map.push_row(vec![u32::MAX; map.cols()]);
+    map.push_col(vec![u32::MAX; map.rows()]);
+
+    let trailheads: Vec<_> = map
+        .indexed_iter()
+        .filter_map(|(pos, &c)| if c == 0 { Some(pos) } else { None })
+        .collect();
+
+    trailheads
+        .iter()
+        .map(|&pos| find_distinct_hiking_trails(&map, pos))
+        .sum()
+}
+
+fn find_distinct_hiking_trails(map: &Grid<u32>, pos: (usize, usize)) -> usize {
+    let new_poses = &[
+        (pos.0 + 1, pos.1),
+        (pos.0 - 1, pos.1),
+        (pos.0, pos.1 + 1),
+        (pos.0, pos.1 - 1),
+    ];
+
+    let mut sum = 0;
+    for new_pos in new_poses {
+        if map[*new_pos] == map[pos] + 1 {
+            if map[*new_pos] == 9 {
+                sum += 1;
+            } else {
+                sum += find_distinct_hiking_trails(map, *new_pos);
+            }
+        }
+    }
+
+    sum
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_input() {
+        let input = include_str!("../test_input.txt").trim();
+        assert_eq!(process_input(input), 81);
+    }
+}
