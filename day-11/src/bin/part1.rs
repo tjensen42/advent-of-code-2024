3c85efcf414a1542cfeb6ad0e019fadbd9a04b95
Tim Jensen <Tim.Jensen@vector.com>
2024-12-11 08:58:28 +0100

    feat: finish day-11

--- day-11/src/bin/part1.rs
+++ day-11/src/bin/part1.rs
@@ -4,7 +4,41 @@ fn main() {
 }
 
 fn process_input(input: &str) -> usize {
-    43
+    let mut stones: Vec<usize> = input
+        .split_whitespace()
+        .map(|n| n.parse().unwrap())
+        .collect();
+
+    for _ in 0..25 {
+        let mut tmp_stones = Vec::with_capacity(stones.len() * 2);
+
+        for stone in stones.into_iter() {
+            match (stone, number_of_digits(stone)) {
+                (0, _) => tmp_stones.push(1),
+                (num, digits) if digits % 2 == 0 => {
+                    let splitted = split_num(num, digits);
+                    tmp_stones.push(splitted.0);
+                    tmp_stones.push(splitted.1);
+                }
+                _ => tmp_stones.push(stone * 2024),
+            }
+        }
+
+        stones = tmp_stones;
+    }
+
+    stones.len()
+}
+
+fn number_of_digits(num: usize) -> usize {
+    (0..).take_while(|n| 10_usize.pow(*n) <= num).count()
+}
+
+fn split_num(num: usize, digits: usize) -> (usize, usize) {
+    let num_str = num.to_string();
+    let splitted = num_str.split_at(digits / 2);
+
+    (splitted.0.parse().unwrap(), splitted.1.parse().unwrap_or(0))
 }
 
 #[cfg(test)]
@@ -14,6 +48,6 @@ mod test {
     #[test]
     fn test_input() {
         let input = include_str!("../test_input.txt").trim();
-        assert_eq!(process_input(input), 42);
+        assert_eq!(process_input(input), 55312);
     }
 }
--- day-11/src/bin/part2.rs
+++ day-11/src/bin/part2.rs
@@ -1,19 +1,71 @@
+use std::collections::HashMap;
+
 fn main() {
     let input = include_str!("../input.txt").trim();
     println!("Part 2: {}", process_input(input));
 }
 
 fn process_input(input: &str) -> usize {
-    43
+    let stones: Vec<usize> = input
+        .split_whitespace()
+        .map(|n| n.parse().unwrap())
+        .collect();
+
+    stones
+        .iter()
+        .map(|&stone| stones_count_after_blinks(stone, 75, &mut HashMap::new()))
+        .sum()
 }
 
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_input() {
-        let input = include_str!("../test_input.txt").trim();
-        assert_eq!(process_input(input), 42);
+fn stones_count_after_blinks(
+    stone: usize,
+    blinks_left: usize,
+    cache: &mut HashMap<(usize, usize), usize>,
+) -> usize {
+    if blinks_left == 0 {
+        return 1;
     }
+
+    if let Some(&count) = cache.get(&(stone, blinks_left)) {
+        return count;
+    }
+
+    let count;
+    let digits = count_digits_of_num(stone);
+
+    if stone == 0 {
+        count = stones_count_after_blinks(1, blinks_left - 1, cache);
+    } else if digits % 2 == 0 {
+        let (left, right) = split_num(stone, digits);
+        count = stones_count_after_blinks(left, blinks_left - 1, cache)
+            + stones_count_after_blinks(right, blinks_left - 1, cache);
+    } else {
+        count = stones_count_after_blinks(stone * 2024, blinks_left - 1, cache);
+    }
+
+    cache.insert((stone, blinks_left), count);
+
+    return count;
+}
+
+fn count_digits_of_num(num: usize) -> usize {
+    (0..).take_while(|n| 10_usize.pow(*n) <= num).count()
+}
+
+fn split_num(num: usize, digits: usize) -> (usize, usize) {
+    let mut left_part = 0;
+    let mut right_part = 0;
+    let half_digits = digits / 2;
+
+    for i in 0..digits {
+        let pow_10_i = 10_usize.pow(i as u32);
+        let digit = (num / pow_10_i) % 10;
+        if i < half_digits {
+            left_part += digit * pow_10_i;
+        } else {
+            right_part += digit * 10_usize.pow((i - half_digits) as u32);
+        }
+    }
+
+    (left_part, right_part)
 }
